c5e0228fc02187ee7e5a79cfa39b3d30a75454d2
Hernat <tokyhernat@gmail.com>
2022-04-21 16:43:13 +0300

    crud en utilisant jQuery

---
+++ src/Controller/MatterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MatterController extends AbstractController
+{
+    #[Route('/matter', name: 'matter')]
+    public function index(): Response
+    {
+        return $this->render('matter/index.html.twig', [
+            'controller_name' => 'MatterController',
+        ]);
+    }
+}
